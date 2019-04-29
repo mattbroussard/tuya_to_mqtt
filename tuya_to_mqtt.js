746429ca0e6f18086a3c8448a49b30f8849af580
Matt Broussard <matt@mattb.io>
2019-04-28 19:10:32 -0700

    add logging

--- tuya_to_mqtt.js
+++ tuya_to_mqtt.js
@@ -1,4 +1,9 @@
 
+const debugModule = require('debug');
+debugModule.enable('*');
+const debug = debugModule('tuya_to_mqtt');
+
+const _ = require('lodash');
 const CloudTuya = require('cloudtuya');
 const mqtt = require('mqtt');
 const config = require('config');
@@ -7,4 +12,17 @@ const tuyaConfig = config.get('tuya');
 const mqttConfig = config.get('mqtt');
 const devices = config.get('devices');
 
-console.log(devices);
+const state = _
+  .chain(devices)
+  .keyBy('deviceId')
+  .mapValues(val => ({
+    ...val,
+    state: false,
+  }))
+  .value();
+
+async function main() {
+  debug(state);
+}
+
+main();
